0e3e9227d84332d99a40227da773e6b9958a2eee
benidama <benidama02@gmail.com>
2024-12-12 20:23:33 +0000

    callback function

--- api.js
+++ api.js
@@ -15,3 +15,32 @@ async function getFile() {
 }
 
 getFile();
+
+function func1(a, b) {
+  document.getElementById("callBack").innerHTML = a + b;
+}
+
+function func2() {
+  func1(25, 7);
+}
+function func3() {
+  func1(45, 78);
+}
+
+func3();
+func2();
+
+// function myDisplayer(some) {
+//   document.getElementById("demo").innerHTML = some;
+// }
+
+// function myFirst() {
+//   myDisplayer("Hell");
+// }
+
+// function mySecond() {
+//   myDisplayer("Goodbye");
+// }
+
+// mySecond();
+// myFirst();
